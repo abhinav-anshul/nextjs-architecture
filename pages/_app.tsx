a347745e00f3109ec7df3600fbb84298c3a4cd24
abhinav <contact.abhinavanshul@gmail.com>
2023-04-26 17:16:37 +0530

    feat : remove Home css, add minor cosmetic changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,15 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Layout from '../components/Layout';
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
+      <Navbar/> 
       <Component {...pageProps} />
+      <Footer />
     </Layout>
   );
 }
