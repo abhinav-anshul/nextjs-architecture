7985966cc98edfe864eb17c4788a23dc9c719e2f
selengr <reza1997karbakhsh@gmail.com>
2023-07-30 17:02:09 +0330

    fix

---
+++ src/components/common/skeleton/index.ts
@@ -0,0 +1,6 @@
+// export * from './types';
+
+export { default as Skeleton } from './Skeleton';
+export { default as SkeletonMap } from './SkeletonMap';
+export { default as SkeletonPostItem } from './SkeletonPostItem';
+export { default as SkeletonPostDetails } from './SkeletonPostDetails';
